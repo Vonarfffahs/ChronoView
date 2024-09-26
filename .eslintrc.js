afb6014b0d8a8f4c69fe300674a6d1586e3a51dc
Victor <mhpd100.i@gmail.com>
2024-09-27 01:38:10 +0300

    Project Init: Setting up the project and dev tools - initialized packages - setted up prettier, eslint - added pre-commit and pre-push git-hooks

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    'no-var': 'error',
+    'prefer-const': 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
