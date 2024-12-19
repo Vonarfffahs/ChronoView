177a8adfd9dd9ad80735e3483ecfb5da109f8dc5
Victor <mhpd100.i@gmail.com>
2024-12-19 13:16:15 +0200

    API. Map module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database.module';
-import { AuthModule } from './auth/auth.module';
+import { RunningRealityModule } from './running-reality/running-reality.module';
 
 @Module({
   imports: [
@@ -13,7 +13,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     DatabaseModule,
     UsersModule,
-    AuthModule,
+    RunningRealityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
